ad6780e7f07d3d575c35cfbe347b4af717547c72
tutncrusoe <huydinhanhle@gmail.com>
2026-03-10 03:28:19 +0700

    chore: Update default server port to 3000

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ const server = http.createServer((req, res) => {
     });
   });
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`[SERVER] Running at port ${PORT}`);
 });
